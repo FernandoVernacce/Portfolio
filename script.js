9d45e0d3a0ef49dd8c820514140c63597802381c
Fernando Vernacce <fernandovernacce26@gmail.com>
2023-11-16 22:33:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.titulo');
+
+sr.reveal('.subtitulo');
+
+sr.reveal('.sobre');
+
+sr.reveal('.minhafoto');
+
+sr.reveal('.sobremim');
+
+sr.reveal('.tituloskills');
+
+sr.reveal('.descricaoskills');
+
+sr.reveal('.imagemskills');
+
+sr.reveal('.contato');
+
+sr.reveal('.redes');
+
+sr.reveal('.linkedin');
+
+sr.reveal('.github');
